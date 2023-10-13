eb5049a3ba585014fe6ad4fdabfd65da95eb3f31
Zain0-0 <122361692+Zain0-0@users.noreply.github.com>
2023-10-13 17:02:35 +0500

    Add files via upload

---
+++ q2.js
@@ -0,0 +1,10 @@
+function greetUser(firstName, lastName) {
+    const fullName = firstName + ' ' + lastName;
+    alert('Hello, ' + fullName + '!');
+  }
+  
+
+  var userFirstName = 'John';
+  var userLastName = 'Doe';
+  greetUser(userFirstName, userLastName);
+  
